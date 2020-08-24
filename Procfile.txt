88d05e3505444fe5e436bfa352035da6a3f4041f
Snehit Vaddi <snehitvaddi@users.noreply.github.com>
2020-08-25 01:09:40 +0530

    First

---
+++ Procfile.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:797e30d37d86ea88cd3ebb8efb30ea326eb88a88c1c22ecea0c358b2833cce30
+size 40
---
+++ app1.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b0874bb1eb8ac957cdf760254d589b26a6314a5ee5f67ac06841414090fbf16e
+size 1770
