eba2f0cef3d4f478c53b82f2c47b705be3bfbdd8
Snehit Vaddi <snehitvaddi@users.noreply.github.com>
2020-11-29 20:48:34 +0530

    Better namng

--- setup.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:14c2263433d34b4ede2c6ff354885cf9c52a645aca10704fe000240e31610da1
-size 228
---
+++ streamlit/app1.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:65397015ad2c4541804bb3446d97c61b30065b2ba5966aeefa803d2cd8861dab
+size 2023
